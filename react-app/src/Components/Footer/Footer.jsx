44a99905f2d3b2ec553c5c52e0e741f829aa31c6
Niraj Kumar <n1r4jkumar@gmail.com>
2022-10-25 22:32:07 +0530

    screenshot added of the frontend

--- react-app/src/Components/Footer/Footer.jsx
+++ react-app/src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <div class="card mt-5 rounded-0">
+            {/* <div class="card mt-5 rounded-0">
                 <div class="card-header">
                     nirajkumar.in
                 </div>
@@ -13,12 +13,20 @@ const Footer = () => {
                         <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
                     </blockquote>
                 </div>
+            </div> */}
+            <div className="bg-primary d-flex justify-content-between fw-bold text-white p-3 mt-5" >
+                <div>
+                    &copy;
+                    <span> {new Date().getFullYear()}</span>
+                    <span> Niraj Kumar</span>
+
+                </div>
+                <div>
+
+                    <a href="http://www.nirajkumar.in" target={'_blank'} className='list-group-item list-group-item-action'>www.nirajkumar.in</a>
+                </div>
             </div>
-            <div className="bg-primary fw-bold text-white p-3" >
-                &copy;
-                <span> {new Date().getFullYear()}</span>
-                <span> Niraj Kumar</span>
-            </div>
+
         </>
     )
 }
